f52e3aed9306e45951f3fa65c271914632726dac
Kerim <targetbad@hotmail.com>
2022-10-02 16:42:02 +0300

    Initial Commit

---
+++ GoodNewsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNewsAPI
+//
+//  Created by Kerim Civelek on 2.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
